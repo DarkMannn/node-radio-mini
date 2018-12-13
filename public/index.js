8bf4e1572a11855057a5cde4740c69e50d62da0f
DarkMannn <mdarko1993@gmail.com>
2018-12-13 22:52:44 +0100

    Tiny tweak

--- public/index.js
+++
@@ -1,9 +0,0 @@
-// 	const audio = document.getElementById('audio');
-// 	// audio.onemptied = (evt) => {
-// 	// 	// audio.src = '/stream';
-// 	// 	audio.load();
-// 	// };
-// 	setInterval(() => {
-// 		if (audio.networkState === 1) audio.load();
-// 	}, 1000);
-// };
