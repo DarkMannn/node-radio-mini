5eeba9569aefa6d08eee3310d90191bb50adfb20
DarkMannn <mdarko1993@gmail.com>
2020-04-15 07:43:40 +0200

    Clean to app.js file

--- app.js
+++ app.js
@@ -2,9 +2,8 @@
 
 require('dotenv').config();
 
-const Path = require('path');
 const Hapi = require('@hapi/hapi');
-const File = require('@hapi/inert');
+const StaticFilePlugin = require('@hapi/inert');
 const Routes = require('./routes');
 const Engine = require('./engine.js');
 
@@ -15,21 +14,17 @@ void async function startApp() {
             port: process.env.PORT || 8080,
             host: process.env.HOST || 'localhost',
             compression: false,
-            routes: {
-                files: {
-                    relativeTo: Path.join(__dirname, 'public')
-                }
-            }
+            routes: { files: { relativeTo: `${__dirname}/public` } }
         });
-        await server.register(File);
+        await server.register(StaticFilePlugin);
         await server.register(Routes);
-        Engine.startEngine();
 
+        Engine.startEngine();
         await server.start();
-        console.log(`Server running at ${server.info.uri}`);
+        console.log(`Server running at: ${server.info.uri}`);
     }
     catch (err) {
-        console.log(`Server error: ${err}`);
+        console.log(`Server errored with: ${err}`);
         process.exit(1);
     }
 }();
